c62cb68c6626d8834a6423431befa2387ce4cf5d
JoniHintz <sully-the-sevent@web.de>
2017-03-30 22:20:07 +0200

    Delete Test.java
    moved to workInProgress

--- src/de/wlanStein/tmxGlue/Test.java
+++
@@ -1,9 +0,0 @@
-package de.kaguia.tmxGlue;
-import de.kaguia.tmxGlue.Map;
-public class Test{
-    public static void main(String agrs[]){
-        Map map =   new Map("../exterior.tmx");
-        System.out.println(map.isValid());
-    }
-}
-
