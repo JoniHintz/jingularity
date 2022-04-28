10ca65c99e63568a99dab3e2a5054fdcbab6f2be
JoniHintz <jonas.flam@freenet.de>
2022-04-28 13:17:08 +0200

    Update Start.java
    renamed state to strategy

--- src/de/wlanStein/exampleGame/Start.java
+++ src/de/wlanStein/exampleGame/Start.java
@@ -4,7 +4,7 @@ import de.wlanStein.jingularity.Frame;
 public class Start{
     public static void main(String args[]){
        Frame fw = new Frame("Example", 600, 600, 60); 
-       fw.pushState(new Menue(fw));
+       fw.pushStrategy(new Menue(fw));
        fw.run(); 
     }
 }
